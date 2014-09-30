a7cf27fa5a1525c2835a6311a8d61c73f5394d36
Drew Jankowski <andrewkjankowski@gmail.com>
2014-09-30 17:00:52 -0400

    Add function to fix

--- really_useful_fix.rb
+++ really_useful_fix.rb
@@ -7,6 +7,9 @@ class FibFinder
       (-1)**(-n+1)*fib_array(-n)[-n]
     end
   end
+  def self.hell s
+     something = array.new()
+  end
   def self.fib_array(n)
     array = []
     array[0] = 0
