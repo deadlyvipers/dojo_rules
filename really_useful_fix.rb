11572c73613461e56917c173331e998669f14ecc
Drew Jankowski <andrewkjankowski@gmail.com>
2014-09-30 16:02:57 -0400

    Add code to useful fix

--- really_useful_fix.rb
+++ really_useful_fix.rb
@@ -1,1 +1,27 @@
 # This is a really useful fix to the project
+class Fib_Finder
+  def self.fib n
+    if !(n >= 0) then
+      (-1)**(-n+1)*fib_array(-n)[-n]
+    else
+      fib_array(n)[n]
+    end
+  end
+  def self.fib_array n
+    begin
+      array = Array.new
+      array[0] = 0
+      array[1] = 1
+      (n-1).times { |x|
+        array[x+2] = array[x+1] + array[x]
+      }
+      return array
+    rescue Exception
+      puts "Something bad happened."
+    end
+  end
+
+  class << self
+    alias :fibonacci :fib
+  end
+end
